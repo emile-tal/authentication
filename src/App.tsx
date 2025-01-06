ca18ddf3b82c8450de0f4a6ed1fc665f8f38dd70
Emile <emiletal@gmail.com>
2025-01-06 11:41:11 +0100

    Create basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { HomePage } from './pages/HomePage/HomePage'
+import { LandingPage } from './pages/LandingPage/LandingPage'
 import { LoginPage } from './pages/LoginPage/LoginPage'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/home' element={<HomePage />} />
       </Routes>
